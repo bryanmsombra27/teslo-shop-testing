8bb9216c883a3badee794aec870c05b4b68de22c
bryanmsombra27 <bryanjm96@gmail.com>
2025-11-10 13:15:34 -0600

    adding testing for raw headers decorator and role protected decorator

---
+++ src/auth/decorators/raw-headers.decorator.spec.ts
@@ -0,0 +1,31 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { getRawHeaders } from './raw-headers.decorator';
+
+jest.mock('@nestjs/common', () => ({
+  //   createParamDecorator: jest.fn().mockImplementation(() => jest.fn()),
+  createParamDecorator: jest.fn(),
+}));
+
+describe('RAW headers decorator', () => {
+  const mockExcecutionContext = {
+    switchToHttp: jest.fn().mockReturnValue({
+      getRequest: jest.fn().mockReturnValue({
+        rawHeaders: ['Authorization', 'Bearer Token', 'User-Agent', 'NestJS'],
+      }),
+    }),
+  } as unknown as ExecutionContext;
+  it('should return the raw headers from the request', () => {
+    const result = getRawHeaders('', mockExcecutionContext);
+
+    expect(result).toEqual([
+      'Authorization',
+      'Bearer Token',
+      'User-Agent',
+      'NestJS',
+    ]);
+  });
+
+  it('should call createParamDecorator with getRawHeader', () => {
+    expect(createParamDecorator).toHaveBeenCalledWith(getRawHeaders);
+  });
+});
---
+++ src/auth/decorators/role-protected.decorator.spec.ts
@@ -0,0 +1,26 @@
+import { SetMetadata } from '@nestjs/common';
+import { ValidRoles } from '../interfaces';
+import { META_ROLES, RoleProtected } from './role-protected.decorator';
+
+jest.mock('@nestjs/common', () => ({
+  //   SetMetadata: jest.fn().mockImplementation((key, value) => ({
+  //     key,
+  //     value,
+  //   })),
+  SetMetadata: jest.fn(),
+}));
+
+describe('Role protected decorator', () => {
+  it('Should set medatada with correct values', () => {
+    const roles = [ValidRoles.admin, ValidRoles.user];
+
+    const result = RoleProtected(...roles);
+
+    expect(SetMetadata).toHaveBeenCalled();
+    expect(SetMetadata).toHaveBeenCalledWith(META_ROLES, roles);
+    // expect(result).toEqual({
+    //   key: META_ROLES,
+    //   value: roles,
+    // });
+  });
+});
